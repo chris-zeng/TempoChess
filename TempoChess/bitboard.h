8f1d79e80abb2a8b875b5a947b7b3aedb5714d03
chris <chris.dz@gmail.com>
2019-10-16 16:20:37 -0400

    'updates'

--- TempoChess/bitboard.h
+++ TempoChess/bitboard.h
@@ -6,17 +6,17 @@
 
 namespace BitBoards{
 	constexpr Bitboard AllSquares = ~Bitboard(0);
-	constexpr Bitboard WhiteSquares = 0xAA55AA55AA55AA55ULL;
-	constexpr Bitboard BlackSquares = ~WhiteSquares;
+	constexpr Bitboard BlackSquares = 0xAA55AA55AA55AA55ULL;
+	constexpr Bitboard WhiteSquares = ~BlackSquares;
 
-	constexpr Bitboard FileABB = 0x8080808080808080ULL;
-	constexpr Bitboard FileBBB = 0x8080808080808080ULL>>1;
-	constexpr Bitboard FileCBB = 0x8080808080808080ULL>>2;
-	constexpr Bitboard FileDBB = 0x8080808080808080ULL>>3;
-	constexpr Bitboard FileEBB = 0x8080808080808080ULL>>4;
-	constexpr Bitboard FileFBB = 0x8080808080808080ULL>>5;
-	constexpr Bitboard FileGBB = 0x8080808080808080ULL>>6;
-	constexpr Bitboard FileHBB = 0x8080808080808080ULL>>7;
+	constexpr Bitboard FileABB = 0x0101010101010101ULL;
+	constexpr Bitboard FileBBB = 0x0101010101010101ULL << 1;
+	constexpr Bitboard FileCBB = 0x0101010101010101ULL << 2;
+	constexpr Bitboard FileDBB = 0x0101010101010101ULL << 3;
+	constexpr Bitboard FileEBB = 0x0101010101010101ULL << 4;
+	constexpr Bitboard FileFBB = 0x0101010101010101ULL << 5;
+	constexpr Bitboard FileGBB = 0x0101010101010101ULL << 6;
+	constexpr Bitboard FileHBB = 0x0101010101010101ULL << 7;
 
 	constexpr Bitboard Rank1BB = 0xFFULL;
 	constexpr Bitboard Rank2BB = 0xFFULL<<8;
